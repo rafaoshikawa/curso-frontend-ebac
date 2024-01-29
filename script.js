79adc6dd8001efcde5be4760f185a7b052e0acf2
rafaoshikawa <rafael.oshikawa@hotmail.com>
2024-01-29 20:09:56 -0300

    exercicio refatorado

--- script.js
+++ script.js
@@ -1,55 +1,31 @@
-$(document).ready(function () {
-  // Oculta o menu hamburguer inicialmente
-  $(".menu-hamburguer").hide();
+  $(document).ready(function () { 
 
-  // Toggle do menu hamburguer ao clicar na linha
-  $(".menu-hamburguer-line").click(function () {
-    $(".menu-hamburguer").fadeToggle();
-  });
+		//aplicando mascaras
+		$("#telefone").mask("(00) 00000-0000");
+		$("#cpf").mask("000.000.000-00");
 
-  // Fechar o menu se clicar no X
-  $(".menu-close").click(function () {
-    $(".menu-hamburguer").fadeOut();
-  });
+		//validando o form
+		$("form").validate({
+			rules: {
+				nome: {
+					required: true,
+				},
+				email: {
+					required: true,
+					email: true,
+				},
+				telefone: {
+					required: true,
+				},
+			},
+			messages: {
+				nome: "Por favor, insira o seu nome",
+				email: "Por favor, insira o seu email",
+				telefone: "Por favor, insira o seu telefone",
+				cpf: "Por favor, insira o seu CPF",
+			},
+		}); 
 
-  // Fechar o menu se clicar em um link
-  $(".menu-hamburguer a").click(function () {
-    $(".menu-hamburguer").fadeOut();
-  });
 
-  // Aplicar a máscara ao campo de telefone usando a classe
-  $(".telefone").mask("(00) 00000-0000");
-
-  //validar o formulário, opcional, pois podemos usar o required no html
-/*   $("form").validate({
-    rules: {
-      nome: {
-        required: true,
-      },
-      email: {
-        required: true,
-        email: true,
-      },
-      telefone: {
-        required: true,
-      },
-    },
-    messages: {
-      nome: "Por favor, insira o seu nome",
-      email: "Por favor, insira o seu email",
-      email: "Por favor, insira o seu telefone",
-    },
-  }); */
-
-  $(".produtos-card a").click(function () {
-    const destino = $("#produto");
-    const nomeProduto = $(this).parent().find("p").text();
-
-    $("#produto").val(nomeProduto);
-
-    $("html").animate(
-      {
-        scrollTop: destino.offset().top
-      },1000);
-  });
-});
+	});
+	// Aplicar a máscara ao campo de telefone usando a classe
